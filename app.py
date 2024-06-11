6cad0283be4882b8a15b61b7b221141d52c98f2a
Intan Amelia <84213559+IntanAmelia@users.noreply.github.com>
2024-06-11 08:16:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def main():
             y_test = y_test.round(2)
             epsilon = 1e-6
             mask = y_test != 0
-            nilai_mape_uji = np.mean(np.abs((y_test[mask] - df_prediksi[mask]) / (y_test[mask] + epsilon))) * 100
+            nilai_mape_uji = np.mean(np.abs((y_test[mask] - df_prediksi_de[mask]) / (y_test[mask] + epsilon))) * 100
             nilai_mape_uji = nilai_mape_uji.round(2)
             st.write('MAPE : ')
             st.write(nilai_mape_uji)
