a4e482bcde248dd026eb8e118d20cd8f9d7d3871
Intan Amelia <84213559+IntanAmelia@users.noreply.github.com>
2024-06-25 18:47:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -91,17 +91,72 @@ elif menu == "Normalisasi Data":
         scaled_data = scaler.fit_transform(df_imputed[['RR_Imputed']])
         df_imputed['Normalisasi'] = scaled_data
         df_normalisasi = df_imputed[['RR_Imputed','Normalisasi']]
-        st.session_state.scaled_data = df_normalisasi
+        st.session_state.scaled_data = df_imputed
         st.write('Data setelah dilakukan normalisasi :')
         st.write(df_normalisasi)
     else:
         st.write('Silahkan masukkan dataset terlebih dahulu')
 elif menu == "Model LSTM":
-    st.header("Contact Us")
-    st.write("Get in touch with us here.")
+    df_imputed = st.session_state.df_imputed
+    if df_imputed is not None:
+        epochs = st.number_input("Masukkan nilai epoch:", min_value=1, max_value=100, value=25)
+        learning_rate = st.number_input("Masukkan nilai learning rate:", min_value=0.0001, max_value=0.01, value=0.01)
+        time_step = st.number_input("Masukkan nilai time step:", min_value=25, max_value=100, value=25)
+        split_data = st.number_input("Masukkan nilai data train:", min_value=0.5, max_value=0.9, value=0.7)
+        # Interpolating outliers
+        valid_indices = df_imputed[~df_imputed['Outlier']].index
+        data_valid = df_imputed.loc[valid_indices]
+        # Scale valid data for prediction
+        scaled_valid_data = scaler.transform(data_valid[['RR']])
+
+        # Pembagian data
+        values = scaled_valid_data
+        training_data_len = math.ceil(len(values) * split_data)
+        train_data = scaled_data[0:training_data_len, :]
+
+        x_train = []
+        y_train = []
+
+        for i in range(time_steps, len(train_data)):
+            x_train.append(train_data[i - time_steps:i, 0])
+            y_train.append(train_data[i, 0])
+
+        x_train, y_train = np.array(x_train), np.array(y_train)
+        x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+        test_data = scaled_data[training_data_len - time_steps:, :]
+        x_test = []
+        y_test = []
+
+        for i in range(time_steps, len(test_data)):
+            x_test.append(test_data[i - time_steps:i, 0])
+            y_test.append(test_data[i, 0])
+
+        x_test, y_test = np.array(x_test), np.array(y_test)
+        x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+        st.session_state.x_train = x_train
+        st.session_state.x_test = x_test
+        st.session_state.y_train = y_train
+        st.session_state.y_test = y_test
+        def build_and_train_lstm(x_train, y_train, x_test, y_test, epochs, learning_rate):
+            model = Sequential()
+            model.add(LSTM(100, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+            model.add(LSTM(100))
+            model.add(Dense(1))
+            optimizer = Adam(learning_rate=learning_rate)
+            model.compile(optimizer=optimizer, loss='mean_squared_error')
+            model.fit(x_train, y_train, batch_size=32, epochs=epochs, verbose=1)
+            return model
+        model = build_and_train_lstm(x_train, y_train, x_test, y_test, epochs, learning_rate)
+        st.session_state.model = model
+    else:
+        st.write('SIlahkan melakukan proses normalisasi data terlebih dahulu.')
 elif menu == "Prediksi LSTM":
-    st.header("Settings")
-    st.write("Configure your preferences here.")
+    if st.session_state.x_train is not None and st.session_state.x_test is not None and st.session_state.y_train is not None and st.session_state.y_test is not None and st.session_state.model is not None:
+        test_predictions = model.predict(x_test)
+        test_predictions_data = scaler.inverse_transform(test_predictions)
+        st.write('Hasil Prediksi :')
+        st.write(test_predictions_data)
 elif menu == "Implementasi":
     st.header("Contact Us")
     st.write("Get in touch with us here.")
