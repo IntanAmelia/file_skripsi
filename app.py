04b19b6bf5b1c172b57e95e43b035924f895ea3a
Intan Amelia <84213559+IntanAmelia@users.noreply.github.com>
2024-07-03 13:12:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -82,17 +82,18 @@ elif menu == "Deteksi Outlier Menggunakan IQR":
         st.write('Dataset yang termasuk outlier :')
         st.dataframe(df_imputed[['interpolasi', 'Outlier']])
         # Replace outliers with linear interpolation values
+        df_imputed['interpolasi outlier'] = df_imputed['interpolasi'].copy()
         for i, is_outlier in enumerate(outliers):
             if is_outlier:
                 if i == 0:
                     # If the first element is an outlier, replace it with the next value
-                    df_imputed[i] = df_imputed['interpolasi'].iloc[i+1]
+                    df_imputed['interpolasi outlier'][i] = df_imputed['interpolasi'].iloc[i+1]
                 elif i == len(df_imputed['interpolasi']) - 1:
                     # If the last element is an outlier, replace it with the previous value
-                    df_imputed[i] = df_imputed['interpolasi'].iloc[i-1]
+                    df_imputed['interpolasi outlier'][i] = df_imputed['interpolasi'].iloc[i-1]
                 else:
                     # For other elements, replace with linear interpolation
-                    df_imputed[i] = (df_imputed['interpolasi'].iloc[i-1] + df_imputed['interpolasi'].iloc[i+1]) / 2
+                    df_imputed['interpolasi outlier'][i] = (df_imputed['interpolasi'].iloc[i-1] + df_imputed['interpolasi'].iloc[i+1]) / 2
         st.session_state.df_imputed = df_imputed
         st.write('Data setelah dilakukan interpolasi :')
         st.dataframe(df_imputed)
