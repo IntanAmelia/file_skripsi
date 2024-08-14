80d077b8962887aa07d0780dafd6450937d58822
Intan Amelia <84213559+IntanAmelia@users.noreply.github.com>
2024-08-14 16:08:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -114,6 +114,8 @@ elif menu == "Prediksi LSTM":
         st.write(df['RR'][1534:])
         st.write(test_predictions)
         if len(data_asli) == len(test_predictions):
+            st.write(np.isnan(st.session_state.df['RR'][1534:]).sum())
+            st.write(np.isnan(test_predictions).sum())
             rmse = np.sqrt(np.mean((st.session_state.df['RR'][1534:] - test_predictions) ** 2))
         else:
             st.write('Error: Mismatch in data lengths for RMSE calculation.')
