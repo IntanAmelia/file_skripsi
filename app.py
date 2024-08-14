d5158dbccbc59366af8251239deb6bde0233fdd1
Intan Amelia <84213559+IntanAmelia@users.noreply.github.com>
2024-08-15 02:28:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ elif menu == "Model LSTM":
 elif menu == "Prediksi LSTM":
     if st.session_state.model is not None and st.session_state.scaler is not None and st.session_state.scaled_data is not None:
         test_predictions = pd.read_csv('predictions_splitdata_0.9_epochs_100_lr_0.01_ts_25.csv')
-        test_predictions_data = test_prediction.values.flatten()
+        test_predictions_data = test_predictions.values.flatten()
         st.session_state.test_predictions = test_predictions
         st.write('Hasil Prediksi Data Uji:')
         st.write(test_predictions)
